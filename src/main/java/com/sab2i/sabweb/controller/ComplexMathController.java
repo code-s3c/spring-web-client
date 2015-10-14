7637b9407731769c103e0e1b24282debf22f9c70
martinsa <martinsa@HOEDUS-SAB1.fontenay.sab2i.local>
2015-10-14 12:22:28 +0200

    configuration and dependencies added

--- src/main/java/com/sab2i/sabweb/controller/ComplexMathController.java
+++ src/main/java/com/sab2i/sabweb/controller/ComplexMathController.java
@@ -5,10 +5,14 @@
  */
 package com.sab2i.sabweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import com.sab2i.demo.service.IComplexMathService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -17,9 +21,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ComplexMathController {
     
+    @Autowired
+    private IComplexMathService calculator;
+    
     @RequestMapping(path="/welcome.do", method = RequestMethod.GET)
     public ModelAndView hello() {
-        String message = "Hello World!";
+        String message = String.valueOf(calculator.doComplexMath(10, 5));
         ModelAndView modelAndView = new ModelAndView("welcome");
         modelAndView.addObject("message", message);
         return modelAndView;
